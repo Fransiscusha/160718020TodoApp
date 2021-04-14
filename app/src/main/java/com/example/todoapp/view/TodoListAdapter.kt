079c0ab2fd6e12622c771650e209c8ab9db01c28
Fransiscus Hartanto <s160718020@student.ubaya.ac.id>
2021-04-14 15:01:36 +0700

    week8

---
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.todoapp.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
+import com.example.todoapp.model.Todo
+import kotlinx.android.synthetic.main.todo_item_layout.view.*
+
+class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick:(Any) -> Unit):RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder>() {
+    class TodoListViewHolder(var view: View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.todo_item_layout, parent, false)
+
+        return TodoListViewHolder(view)
+    }
+
+    fun updateTodoList(newTodoList:List<Todo>){
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
+        holder.view.checkTask.text = todoList[position].title
+        holder.view.checkTask.setOnCheckedChangeListener { compoundButton, b ->
+            adapterOnClick(todoList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+}
